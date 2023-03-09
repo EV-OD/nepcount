4b4f47988e0cc58d33d8e78a7c2d058b17c3b3cd
EV-OD <evod599@gmail.com>
2023-03-09 13:17:05 +0545

    removed capatalized tailwind class

--- components/items/itemView.tsx
+++ components/items/itemView.tsx
@@ -9,7 +9,7 @@ type ItemViewType = {
 function ItemView({ item }: ItemViewType) {
     return (
         <tr className='border-[1px] border-slate-200'>
-            <td className="text-black py-2 border-[1px] px-2 border-gray-300 capitalize">{item.name}</td>
+            <td className="text-black py-2 border-[1px] px-2 border-gray-300">{item.name}</td>
             <td className="text-black py-2 border-[1px] px-2 border-gray-300">{item.quantity}</td>
             <td className="text-black py-2 border-[1px] px-2 border-gray-300">{item.costprice}</td>
             <td className="text-black py-2 border-[1px] px-2 border-gray-300">{item.sellingprice}</td>
